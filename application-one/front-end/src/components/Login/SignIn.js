2175d720edbb0c6a5493d180f654612721fc55ae
Layla Dai <Layla_Dai@epam.com>
2020-01-09 10:03:40 +0800

    modified sign in ui

--- application-one/front-end/src/components/Login/SignIn.js
+++ application-one/front-end/src/components/Login/SignIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, TextField, ThemeProvider } from '@material-ui/core';
+import { Button, TextField, FormControl, FormHelperText, ThemeProvider } from '@material-ui/core';
 import theme from '../styles';
 import './styles.css';
 
@@ -17,9 +17,24 @@ class SignIn extends React.Component {
     return (
       <ThemeProvider theme={theme}>
         <form className="forms-container">
-          <TextField id="email" label="E-mail" type="email" color="secondary" />
-          <TextField id="password" label="Password" type="password" color="secondary" />
-          <Button type="submit"  variant="contained" color="primary" disableElevation>Login</Button>
+          <TextField 
+            required 
+            id="email" 
+            label="E-mail" 
+            type="email" 
+            color="secondary" 
+          />
+          <FormControl>
+            <TextField
+              required 
+              id="password" 
+              label="Password" 
+              type="password" 
+              color="secondary" 
+            />
+            <FormHelperText error></FormHelperText>
+          </FormControl>
+          <Button className="login-btn" type="submit"  variant="contained" color="primary" disableElevation>Login</Button>
           <Button color="secondary" onClick={() => this.setLoginStatus()}>Sign Up</Button>
         </form>
       </ThemeProvider>
